35dff09e59369b39748dd4eb1920309bd4b57baf
Simon Stiefel <simonboots@me.com>
2015-08-23 22:31:11 -0700

    Some code cleanup and commenting

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -10,11 +10,21 @@ import Foundation
 
 class CalculatorBrain
 {
+    // Available operation types
     private enum Op: Printable {
+        // Basic operand, e.g. 1.234
         case Operand(Double)
+        
+        // Constant operand, e.g. π
         case ConstantOperand(String, Double)
+        
+        // User-settable variable operand, e.g. M
         case VariableOperand(String)
+        
+        // Unary operation, e.g. cos()
         case UnaryOperation(String, Double -> Double)
+        
+        // Binary operation, e.g. +
         case BinaryOperation(String, (Double, Double) -> Double)
         
         var description: String {
@@ -36,9 +46,19 @@ class CalculatorBrain
     }
     
     weak var delegate: CalculatorBrainDelegate?
+    
+    // Operation and operand stack
     private var opStack = [Op]()
+    
+    // Array of known operations and operands
     private var knownOps = [String:Op]()
-    private var opsPrecedences = [String:Int]()
+    
+    // Binary operation precedence map
+    private var binaryOperationPrecendences = [String:Int]()
+    
+    // Maps user-defined variables to their values
+    var variableValues = [String:Double]()
+    
     private var descriptionNumberFormatter: NSNumberFormatter
     
     init() {
@@ -52,15 +72,18 @@ class CalculatorBrain
         knownOps["±"] = Op.UnaryOperation("±") { $0 * -1 }
         knownOps["π"] = Op.ConstantOperand("π", M_PI)
         
-        opsPrecedences["×"] = 1
-        opsPrecedences["÷"] = 1
-        opsPrecedences["+"] = 2
-        opsPrecedences["−"] = 2
+        // "×" and "÷" have higher precendence than "+" and "−"
+        binaryOperationPrecendences["×"] = 1
+        binaryOperationPrecendences["÷"] = 1
+        binaryOperationPrecendences["+"] = 2
+        binaryOperationPrecendences["−"] = 2
         
         descriptionNumberFormatter = NSNumberFormatter()
         descriptionNumberFormatter.numberStyle = .DecimalStyle
     }
     
+    // Recursively evaluates the contents of ops
+    // Returns optional result as Double and the remaining operations and operands that couldn't be evaluated
     private func evaluate(ops: [Op]) -> (result: Double?, remainingOps: [Op])
     {
         if !ops.isEmpty {
@@ -80,37 +103,49 @@ class CalculatorBrain
                 }
                 
             case .UnaryOperation(_, let operation):
+                // Unary operation takes one parameter that we first need to evaluate based on the remaining ops
                 let operandEvaluation = evaluate(remainingOps)
                 if let operand = operandEvaluation.result {
+                    // Result is operation on operand
+                    // Remaining ops are the remaining ops from the operand evaluation
                     return (operation(operand), operandEvaluation.remainingOps)
                 }
                 
             case .BinaryOperation(_, let operation):
-                let op1Evaluation = evaluate(remainingOps)
-                if let operand1 = op1Evaluation.result {
-                    let op2Evaluation = evaluate(op1Evaluation.remainingOps)
-                    if let operand2 = op2Evaluation.result {
-                        return (operation(operand1, operand2), op2Evaluation.remainingOps)
+                // Binary operation takes two parameters that we first need to evaluate based on the remaining ops
+                let operand1Evaluation = evaluate(remainingOps)
+                if let operand1 = operand1Evaluation.result {
+                    let operand2Evaluation = evaluate(operand1Evaluation.remainingOps)
+                    if let operand2 = operand2Evaluation.result {
+                        // Result is operation on operand1 and operand2
+                        // Remaining ops are the remaining ops from the second operand evaluation
+                        return (operation(operand1, operand2), operand2Evaluation.remainingOps)
                     }
                 }
             }
         }
         
+        // ops is empty
         return (nil, ops)
     }
     
+    // List of stack contents
     var history: [String] {
         get {
             return opStack.map { $0.description }
         }
     }
     
+    // Returns a string that describes the current contents on the stack as an expression
+    // If the contents of a stack cannot be converted to a single expression, multiple expressions are
+    // concatenated with a comma.
     var description: String? {
         get {
             var remainingOps = opStack
             var descriptions = [String]()
             var foundDescription = false
             
+            // Build expressions until all operations and operands in the stack are used
             do {
                 let descriptionData = buildDescription(remainingOps)
                 if let description = descriptionData.resultString {
@@ -124,10 +159,13 @@ class CalculatorBrain
                 
             } while (foundDescription)
             
+            // Concatenate expressions with commas
             return (descriptions.reverse() as NSArray).componentsJoinedByString(",")
         }
     }
     
+    // This method is similar to the evaluate() method above in that it recusively analyzes the stack
+    // but instead of evaluating the operations and operands it builds a readable expression in the form of 'cos(1+(4*π))'
     private func buildDescription(ops: [Op]) -> (resultString: String?, remainingOps: [Op], precedence: Int?) {
         if !ops.isEmpty {
             var remainingOps = ops
@@ -144,68 +182,81 @@ class CalculatorBrain
                 return (symbol, remainingOps, nil)
                 
             case .UnaryOperation(let symbol, _):
+                // Unary operation takes one parameter that we first need to build based on the remaining ops
                 let operandDescription = buildDescription(remainingOps)
                 if let operand = operandDescription.resultString {
+                    // The expression is 'symbol(operand)'
+                    // Remaining ops are the remaining ops from the operand evaluation
                     return ("\(symbol)(\(operand))", operandDescription.remainingOps, nil)
                 }
                 
             case .BinaryOperation(let symbol, _):
+                // Binary operation takes two parameters that we first need to build based on the remaining ops
                 let op1Description = buildDescription(remainingOps)
                 var op1String = (op1Description.resultString != nil) ? op1Description.resultString! : "?"
                     
                 let op2Description2 = buildDescription(op1Description.remainingOps)
                 var op2String = (op2Description2.resultString != nil) ? op2Description2.resultString! : "?"
                 
-                let selfPrecedence = opsPrecedences[symbol]!
+                let selfPrecedence = binaryOperationPrecendences[symbol]!
                 
+                // Wrap op1String in parenthesis if its precedence is greater than our operation's precendence
                 if let op1Precedence = op1Description.precedence {
                     if (op1Precedence > selfPrecedence) {
                         op1String = "(\(op1String))"
                     }
                 }
-                
+
+                // Wrap op2String in parenthesis if its precedence is greater than our operation's precendence
                 if let op2Precedence = op2Description2.precedence {
                     if (op2Precedence > selfPrecedence) {
                         op2String = "(\(op2String))"
                     }
                 }
                 
-                return ("\(op2String)\(symbol)\(op1String)", op2Description2.remainingOps, opsPrecedences[symbol])
+                // The expression is 'op2String operation op1String'
+                // Remaining ops are the remaining ops from the second operand evaluation
+                return ("\(op2String)\(symbol)\(op1String)", op2Description2.remainingOps, selfPrecedence)
             }
         }
 
+        // Ops is empty
         return (nil, ops, nil)
     }
     
-    var variableValues = [String:Double]()
-    
+    // Reset the op stack
     func reset() {
         opStack.removeAll(keepCapacity: false)
         notifyStackChange()
     }
     
+    // Reset user-defined variables
     func resetVariables() {
         variableValues.removeAll(keepCapacity: false)
     }
     
+    // Evaluate opStack
     func evaluate() -> Double? {
         let (result, remainder) = evaluate(opStack)
         println("\(opStack) = \(result) with \(remainder) left over")
         return result
     }
     
+    // Push operand onto opStack and return evaluated result
     func pushOperand(operand: Double) -> Double? {
         opStack.append(Op.Operand(operand))
         notifyStackChange()
         return evaluate()
     }
-    
+
+    // Push variable operand (string) onto opStack and return evaluated result
     func pushOperand(symbol: String) -> Double? {
         opStack.append(Op.VariableOperand(symbol))
         notifyStackChange()
         return evaluate()
     }
     
+    // Perform operation and return evaluated result
     func performOperation(symbol: String) -> Double? {
         if let operation = knownOps[symbol] {
             opStack.append(operation)
@@ -215,6 +266,7 @@ class CalculatorBrain
         return evaluate()
     }
     
+    // Pop operation or operand from opStack
     func popOperationOrOperand() -> Double? {
         if opStack.count > 0 {
             opStack.removeLast()
@@ -224,6 +276,7 @@ class CalculatorBrain
         return evaluate()
     }
     
+    // Stack ends in an operation
     func didFinishOperation() -> Bool {
         if let lastOp = opStack.last {
             switch lastOp {
@@ -238,6 +291,7 @@ class CalculatorBrain
         return false
     }
     
+    // Notifies delegate about stack changes
     func notifyStackChange() {
         if let delegate = self.delegate {
             delegate.calculatorBrainDidUpdateStack(self)
--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,21 +8,30 @@
 
 import UIKit
 
+// This is the main view controller representing the main UI.
+
 class ViewController: UIViewController, CalculatorBrainDelegate
 {
     @IBOutlet weak var display: UILabel!
-    @IBOutlet weak var historyLabel: UILabel!
+    @IBOutlet weak var expressionLabel: UILabel!
     @IBOutlet weak var undoButton: UIButton!
     
     private var displayNumberFormatter = NSNumberFormatter()
-    var userIsInTheMiddleOfTypingANumber = false
+    
+    // Tracks whether the user is in the middle of typing a number
+    private var userIsInTheMiddleOfTypingANumber = false
     {
         didSet {
             updateUndoButton()
         }
     }
-    var userTypedNumber: String?
-    var brain = CalculatorBrain()
+    
+    // Temporary variable that holds the user-entered number (while userIsInTheMiddleOfTypingANumber is true)
+    // Once the number has been fully entered, it is pushed onto the stack
+    private var userTypedNumber: String?
+    
+    // Model that does all the computation
+    private var brain = CalculatorBrain()
     
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
@@ -44,23 +53,27 @@ class ViewController: UIViewController, CalculatorBrainDelegate
         
         if userIsInTheMiddleOfTypingANumber {
             if let typedNumber = userTypedNumber {
-                // Handle decimal point (only allow one)
+                // Allow only one decimal point
                 if digit == "." && typedNumber.rangeOfString(".", options: nil, range: nil, locale: nil) != nil {
                     return
                 }
                 
+                // Append user typed number
                 userTypedNumber = typedNumber + digit
             }
         } else {
+            // User started entering a number
             userTypedNumber = digit
             userIsInTheMiddleOfTypingANumber = true
         }
         
+        // Update display
         displayStringValue = userTypedNumber
     }
     
     @IBAction func operate(sender: UIButton) {
         if userIsInTheMiddleOfTypingANumber {
+            // Commit user-entered before performing operation
             enter()
         }
         
@@ -71,47 +84,59 @@ class ViewController: UIViewController, CalculatorBrainDelegate
     
     @IBAction func enter() {
         if let newOperandStringValue = userTypedNumber {
+            // Convert user-entered number (string) to double and push onto stack
             if let newOperandValue = NSNumberFormatter().numberFromString(newOperandStringValue)?.doubleValue {
                 displayValue = brain.pushOperand(newOperandValue)
             }
         }
         
+        // Reset user-entering state
         userIsInTheMiddleOfTypingANumber = false
         userTypedNumber = nil
     }
     
+    // This method acts as both "backspace" and "undo" action depending on whether the user is in the middle of entering a number
     @IBAction func backspace(sender: AnyObject) {
-        
         if userIsInTheMiddleOfTypingANumber {
+            // Backspace
             if let typedNumber = userTypedNumber {
                 if count(typedNumber) > 1 {
+                    // Remove least significant digit from user-entered number
                     userTypedNumber = dropLast(typedNumber)
                 } else {
+                    // Reset user-entered number to 0 and reset state
                     userTypedNumber = "0"
                     userIsInTheMiddleOfTypingANumber = false
                 }
                 
+                // Update display
                 displayStringValue = userTypedNumber
             }
             
         } else {
+            // Undo
             displayValue = brain.popOperationOrOperand()
         }
     }
     
     @IBAction func negate(sender: UIButton) {
         if userIsInTheMiddleOfTypingANumber {
+            // If the user is in the middle of entering a number we can just negate that number
             if let typedNumber = userTypedNumber {
                 let isNegative = typedNumber.hasPrefix("-")
                 if isNegative {
+                    // Remove "-"
                     userTypedNumber = typedNumber.stringByReplacingOccurrencesOfString("-", withString: "", options: .LiteralSearch, range: nil)
                 } else {
+                    // Prepend "-"
                     userTypedNumber = "-" + typedNumber
                 }
                 
+                // Update display
                 displayStringValue = userTypedNumber
             }
         } else {
+            // If the user is NOT entering a number we push the negate operation onto the stack
             operate(sender)
         }
     }
@@ -126,17 +151,24 @@ class ViewController: UIViewController, CalculatorBrainDelegate
     
     @IBAction func storeM() {
         if let currentValue = displayValue {
+            // Set value for "M"
             brain.variableValues["M"] = currentValue
+            
+            // Re-evaluate stack and update display
             displayValue = brain.evaluate()
             userIsInTheMiddleOfTypingANumber = false
         }
     }
     
     @IBAction func pushM() {
+        // Push variable "M"
         brain.pushOperand("M")
+        
+        // Re-evaulate stack and update display
         displayValue = brain.evaluate()
     }
     
+    // String value for display
     var displayStringValue: String? {
         get {
             return display.text
@@ -152,10 +184,12 @@ class ViewController: UIViewController, CalculatorBrainDelegate
         }
     }
     
+    // Double value for display
     var displayValue: Double? {
         get {
             var doubleValue : Double? = nil
             if let displayText = display.text {
+                // Convert string to number
                 if let numberFromDisplayText = NSNumberFormatter().numberFromString(displayText) {
                     doubleValue = numberFromDisplayText.doubleValue
                 }
@@ -166,22 +200,27 @@ class ViewController: UIViewController, CalculatorBrainDelegate
         set {
             var displayValueString = "0"
             if let doubleValue = newValue {
+                // Convert double to formatted string
                 displayValueString = displayNumberFormatter.stringFromNumber(doubleValue)!
             }
             
+            // Update display
             display.text = displayValueString
         }
     }
     
-    private func updateHistory() {
+    // Update the expression label
+    private func updateExpressionLabel() {
         if var historyString = brain.description {
             if (brain.didFinishOperation()) {
+                // If there are no "left-overs" on the stack, append "="
                 historyString = historyString + " ="
             }
 
-            historyLabel.text = historyString
+            expressionLabel.text = historyString
         } else {
-            historyLabel.text = ""
+            // Reset label
+            expressionLabel.text = ""
         }
     }
     
@@ -198,6 +237,6 @@ class ViewController: UIViewController, CalculatorBrainDelegate
     // MARK: CalculatorBrainDelegate Methods
     
     func calculatorBrainDidUpdateStack(brain: CalculatorBrain) {
-        updateHistory()
+        updateExpressionLabel()
     }
 }
